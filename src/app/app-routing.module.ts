7ab90f3546a64c4bb2c5cadaaf9038ad0da045b5
Duc H. Phan <ptdat.thanhdatphan@gmail.com>
2019-01-21 14:42:49 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AccomodationComponent} from './accomodation/accomodation.component';
+import {CelebrationsComponent} from './celebrations/celebrations.component';
+import {FoodBeverageComponent} from './food-beverage/food-beverage.component';
+import {LifestyleComponent} from './lifestyle/lifestyle.component';
+import {OffersComponent} from './offers/offers.component';
+import {WeddingsComponent} from './weddings/weddings.component';
+import {SigninComponent} from './auth/signin/signin.component';
+import {SignupComponent} from './auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accomodation', component: AccomodationComponent},
+  { path: 'celebrations', component: CelebrationsComponent},
+  { path: 'foot-beverage', component: FoodBeverageComponent},
+  { path: 'lifestyle', component: LifestyleComponent},
+  { path: 'offers', component: OffersComponent},
+  { path: 'weddings', component: WeddingsComponent},
+  { path: 'signin', component: SigninComponent},
+  { path: 'signup', component: SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
